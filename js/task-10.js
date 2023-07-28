71ecd8b1359d1e4d52d038bbd17085d9450d036f
Agmund2002 <malysh.andrey2000@gmail.com>
2023-07-29 02:14:19 +0300

    виконав 10 завдання

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const elements = {
+  createBtn: document.querySelector('button[data-create]'),
+  destroyBtn: document.querySelector('button[data-destroy]'),
+  boxes: document.querySelector('#boxes'),
+}
+
+elements.createBtn.addEventListener('click', createBoxes);
+elements.destroyBtn.addEventListener('click', destroyBoxes);
+
+let step = 20;
+
+function createBoxes(evt) {
+  let boxCounter = [];
+
+  const {previousElementSibling} = evt.currentTarget;
+
+  if (previousElementSibling.value <= 0 || previousElementSibling.value > 100) {
+    return alert('It is not possible to create more than 100 and less than 1 item!');
+  }
+
+  for (let i = 1; i <= previousElementSibling.value; i++) {
+    step += 10;
+    boxCounter.push(`<div style="width: ${step}px; height: ${step}px; background-color: ${getRandomHexColor()}; margin: 10px;"></div>`)
+  }
+
+  elements.boxes.insertAdjacentHTML("beforeend", boxCounter.join(''))
+}
+
+function destroyBoxes() {
+  elements.boxes.innerHTML = '';
+  step = 20;
+}
