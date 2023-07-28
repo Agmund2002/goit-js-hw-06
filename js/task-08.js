3a8222dc395009bb7edde19ab6bb08506f4721d0
Agmund2002 <malysh.andrey2000@gmail.com>
2023-07-28 22:20:50 +0300

    виконав 8 завдання

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handlerForm);
+
+function handlerForm(evt) {
+    evt.preventDefault();
+
+    const {email, password} = evt.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        return alert('Please fill in all the fields!');
+    }
+
+    const dataUser = {};
+
+    dataUser[form.firstElementChild.textContent.trim()] = email.value;
+    dataUser[form.firstElementChild.nextElementSibling.textContent.trim()] = password.value;
+
+    console.log(dataUser);
+
+    form.reset();
+}
