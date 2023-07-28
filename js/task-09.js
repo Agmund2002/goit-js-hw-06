49ce4dccef6a26fdcda9c08db1473aad8c2f57fa
Agmund2002 <malysh.andrey2000@gmail.com>
2023-07-28 22:51:30 +0300

    виконав 9 завдання

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+const elements = {
+  box: document.querySelector('.widget'),
+  color: document.querySelector('.color'),
+  btn: document.querySelector('.change-color')
+}
+
+elements.btn.addEventListener('click', handlerChangeColor);
+
+function handlerChangeColor() {
+
+  elements.box.parentElement.style.backgroundColor = getRandomHexColor();
+
+  elements.color.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
