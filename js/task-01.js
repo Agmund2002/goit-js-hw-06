b8f2ec4fbe5cfea313c9e0407f5a26cd87da98bc
Agmund2002 <malysh.andrey2000@gmail.com>
2023-07-28 13:26:18 +0300

    вивів кількість дітей ul в консоль

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+const list = document.querySelector('#categories');
+// console.log(list);
+const childs = list.children;
+// console.log(childs);
+console.log('Number of categories:', childs.length);
