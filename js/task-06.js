5a306bdb98470ac778dbe609779789b8caf74693
Agmund2002 <malysh.andrey2000@gmail.com>
2023-07-29 15:12:51 +0300

    внесені зміни в 6 завдання

--- js/task-06.js
+++ js/task-06.js
@@ -5,14 +5,11 @@ textInput.addEventListener('blur', handlerInput);
 function handlerInput(evt) {
     const {value, dataset, classList} = evt.currentTarget;
 
-    if (value.length > dataset.length) {
-        classList.remove('valid');
-        classList.add('invalid');
-    } else if (value.length <= dataset.length && value.length > 0) {
-        classList.remove('invalid');
+    if (value.length === Number(dataset.length)) {
         classList.add('valid');
-    } else {
         classList.remove('invalid');
+    } else {
+        classList.add('invalid');
         classList.remove('valid');
     }
 }
